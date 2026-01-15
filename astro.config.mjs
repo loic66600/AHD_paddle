91f57fdf08a0847f477099dfad5725d0faeaa805
webmaster66 <web@redwoodpaddle.com>
2026-01-15 16:42:46 +0100

    deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,11 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static'
+  output: 'static',
+  // L'adresse de votre profil GitHub
+  site: 'https://loic66600.github.io',
+  // Le nom exact de votre dépôt GitHub (avec le slash au début)
+  base: '/AHD_paddle',
 });
+
 
